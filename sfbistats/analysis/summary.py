9714c7b4eafabccb248dab1a4f69dd7b1d123e8b
lr <royludo4@hotmail.com>
2016-03-12 15:47:40 +0100

    added 2 per city graphs

--- sfbistats/analysis/summary.py
+++ sfbistats/analysis/summary.py
@@ -117,3 +117,80 @@ def run(job_list, output_dir):
     plt.title(u'Niveau de diplôme requis pour les CDD et CDI', y=1.08)
     plt.savefig(os.path.join(output_dir, 'figure_1_5.png'), bbox_inches='tight')
     plt.close()
+
+    df_city2 = pd.Series(df.city).value_counts()
+    citytype_dict = dict()
+    citysubtype_dict = dict()
+    for i in range(0, len(df)):
+        city = df.city[i].encode('utf8')
+        type = df.contract_type[i].encode('utf8')
+        subtype = df.contract_subtype[i].encode('utf8')
+        if citytype_dict.has_key(city):
+            if citytype_dict[city].has_key(type):
+                citytype_dict[city][type] += 1
+            else:
+                citytype_dict[city][type] = 1
+
+            if citysubtype_dict[city].has_key(subtype):
+                citysubtype_dict[city][subtype] += 1
+            else:
+                citysubtype_dict[city][subtype] = 1
+        else:
+            citytype_dict[city] = dict()
+            citytype_dict[city][type] = 1
+            citysubtype_dict[city] = dict()
+            citysubtype_dict[city][subtype] = 1
+    proportion_dict = {'CDD': {}, 'CDI': {}, u'Thèse': {}, 'Stage': {}, 'Total': {}}
+    count_dict = {'CDD': {}, 'CDI': {}, u'Thèse': {}, 'Stage': {}}
+    subtype_count_dict = {'PR': {}, 'MdC': {}, 'CR': {}, 'IR': {}, 'IE': {}, 'CDI autre': {},
+                          'Post-doc / IR': {}, u'CDD Ingénieur': {}, 'ATER': {}, 'CDD autre': {} }
+    study_level_dict = {'Master': {}, 'PhD': {}, 'Total': {}}
+    for i in range(0,10):
+        city = df_city2.index[i]
+        total = df_city2[i]
+        print str(city)+' '+str(total)
+        #proportion_dict[city] = {'CDD': 0, 'CDI': 0, u'Thèse': 0, 'Stage': 0}
+        for k,v in citytype_dict[city].iteritems():
+            proportion_dict[k.decode('utf8')][city] = citytype_dict[city][k] / float(total)
+            count_dict[k.decode('utf8')][city] = citytype_dict[city][k]
+        proportion_dict['Total'][city] = total
+        for k,v in citysubtype_dict[city].iteritems():
+            if k:
+                subtype_count_dict[k.decode('utf8')][city] = citysubtype_dict[city][k]
+
+        study_level_dict['PhD'][city] = 0
+        study_level_dict['Master'][city] = 0
+        subtotal = 0
+        for k in phd_level:
+            if citysubtype_dict[city].has_key(k.encode('utf8')):
+                study_level_dict['PhD'][city] += citysubtype_dict[city][k.encode('utf8')]
+                subtotal += citysubtype_dict[city][k.encode('utf8')]
+        for k in master_level:
+            if citysubtype_dict[city].has_key(k.encode('utf8')):
+                study_level_dict['Master'][city] += citysubtype_dict[city][k.encode('utf8')]
+                subtotal += citysubtype_dict[city][k.encode('utf8')]
+        study_level_dict['Master'][city] /= float(subtotal)
+        study_level_dict['PhD'][city] /= float(subtotal)
+        study_level_dict['Total'][city] = subtotal
+
+    df_study_level_city = pd.DataFrame(study_level_dict).fillna(0).sort_values(by='Total')
+    ax = df_study_level_city.loc[:, ['Master', 'PhD']].plot(kind='barh', stacked=True, color=colors)
+    ax.set_xlim([0,1])
+    i = 0
+    for total in df_study_level_city['Total']:
+        ax.text(1.02, i-0.15, total)
+        i += 1
+    plt.title('Types de poste', y=1.08)
+    plt.savefig(os.path.join(output_dir, 'figure_1_9.png'), bbox_inches='tight')
+
+    df_proportion_city = pd.DataFrame(proportion_dict).fillna(0).sort_values(by='Total')
+    ax = df_proportion_city.loc[:, ['CDD', 'CDI', 'Stage', u'Thèse']].plot(kind='barh', stacked=True, color=colors)
+    ax.set_xlim([0,1])
+    i = 0
+    for total in df_proportion_city['Total']:
+        ax.text(1.02, i-0.15, total)
+        i += 1
+    plt.title('Types de poste', y=1.08)
+    plt.savefig(os.path.join(output_dir, 'figure_1_10.png'), bbox_inches='tight')
+
+
