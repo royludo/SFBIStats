1e11347219f444bd1164a1a29a3ffae2cd72c36a
royludo <royludo4@hotmail.com>
2016-10-05 20:00:48 +0200

    changed some setup description, fixed some imported package path

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='sfbistats',
-      version='0.1',
-      description='Job stats from SFBI website',
+      version='1.0',
+      description='Data and charts related to the french bioinformatics job market.',
       url='https://github.com/royludo/SFBIStats',
-      author='lroy',
+      author='Ludovic Roy',
       author_email='royludo4@hotmail.com',
       license='DO WHAT THE FUCK YOU WANT',
       packages=['sfbistats',
